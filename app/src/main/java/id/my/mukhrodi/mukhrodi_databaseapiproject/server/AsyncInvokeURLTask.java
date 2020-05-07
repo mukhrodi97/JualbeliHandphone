65ffefdf207958f4edb1ea15032a688e42092701
mukhrodi97 <mukhrodi03@gmail.com>
2020-05-07 15:49:56 +0700

    no message

--- app/src/main/java/id/my/mukhrodi/mukhrodi_databaseapiproject/server/AsyncInvokeURLTask.java
+++ app/src/main/java/id/my/mukhrodi/mukhrodi_databaseapiproject/server/AsyncInvokeURLTask.java
@@ -28,7 +28,7 @@ public class AsyncInvokeURLTask extends AsyncTask<Void, Void, String> {
     public boolean showdialog =false;
     public String message ="Proses Data";
     //Harus diganti dengan IP Server yang digunakan
-    public String url_server ="https://siakad-altie.000webhostapp.com/xphone/";
+    public String url_server ="https://mukhrodi.000webhostapp.com/xphone/";
     @SuppressLint("StaticFieldLeak")
     public Context applicationContext;
     public static interface OnPostExecuteListener{
